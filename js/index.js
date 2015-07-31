d5e25e806b5ab4e893bd1e4c2841871c0d29b42c
Eduardo <elopezm20@gmail.com>
2015-07-31 18:23:17 -0500

    Cambios 1
    app 2

--- js/index.js
+++ js/index.js
@@ -5,6 +5,12 @@ $(window).ready(function(){
 $(window).on("resize", function () {
     $('#background').height($(window).height()-32);
 
-});
+    var dispositivo = navigator.userAgent.toLowerCase();
+
+    if( dispositivo.search(/iphone|ipod|ipad|android/) > -1 ){
+
+        document.location = "http://192.168.43.19/Electronica/Views/otherDevice.html";  }
+
+    });
 
 
